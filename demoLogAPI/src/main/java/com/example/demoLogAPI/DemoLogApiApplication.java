23a7983e3bbcc898d0ac1a6a726daf92786d7c5b
cadmarques <57234813+cadmarques@users.noreply.github.com>
2021-10-29 03:52:50 -0400

    Add files via upload

---
+++ demoLogAPI/src/main/java/com/example/demoLogAPI/DemoLogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoLogAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLogApiApplication.class, args);
+	}
+
+}
---
+++ demoLogAPI/src/test/java/com/example/demoLogAPI/DemoLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoLogAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
